26bba7c27febe6eb104431cbb342c7e468feee76
Arturo7baldipro <153790614+Arturo7baldipro@users.noreply.github.com>
2024-01-16 07:39:08 -0600

    Todo

---
+++ Extraordinario/Interfaces/IHabilidad.cs
@@ -0,0 +1,12 @@
+﻿namespace Extraordinario.Interfaces
+{
+    internal interface IHabilidad
+    {
+        int id { get; set; }
+        string Nombre { get; set; }
+        string clase { get; set; }
+        float nivel_requerido { get; set; }
+
+        bool pasivo { get; set; }
+    }
+}
---
+++ Extraordinario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Extraordinario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
